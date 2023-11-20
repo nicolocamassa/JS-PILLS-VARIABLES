0f20df56d9556883ccde2be1c5c7dd78d6c8626c
nicolocamassa <camassanicolo03@gmail.com>
2023-11-20 10:01:42 +0100

    [UPDATE] Exercise 4 done

--- js/script.js
+++ js/script.js
@@ -4,5 +4,18 @@ console.log(number);
 const PI = 3.14;
 console.log(PI);
 
+/* 
+ERRORE
 PI = 3.1416;
-console.log(PI);
+console.log(PI); */
+
+let radius = 8;
+let circle = radius * 2 * PI;
+console.log(circle);
+
+let name = "Carlo";
+name = "Marco";
+console.log(name);
+
+let greet = "Ciao Mondo!";
+console.log(greet);
